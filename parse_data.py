e35bf3e1fff738f77d7d9fbb07a0ffdf6b6eb923
Fabio Carrara <fabio.blackdragon@gmail.com>
2018-02-21 19:20:00 +0100

    add split selection and data parsing

--- parse_data.py
+++ parse_data.py
@@ -19,10 +19,12 @@ def get_sequences(fle):
 
 def parse_sequence(lines):
     header = lines[0]
-    header_regexp = r'.*\s(\d+)_(\d+)_(\d+)_(\d+)'
+    header_regexp = r'.*\s((\d+)_(\d+)_(\d+)_(\d+))'
     matches = re.match(header_regexp, header)
     assert matches is not None, "Error parsing data header: %s" % header
-    seq_id, action_id, start_frame, duration = map(int, matches.groups())
+    attributes = matches.groups()
+    sample_id = attributes[0]
+    seq_id, action_id, start_frame, duration = map(int, attributes[1:])
     lines = lines[2:]  # discard header
 
     def parse_line(line):
@@ -35,6 +37,7 @@ def parse_sequence(lines):
     data = [parse_line(line) for line in lines]
     data = np.stack(data)
     sequence = dict(
+        id=sample_id,
         seq_id=seq_id,
         action_id=action_id,
         start_frame=start_frame,
@@ -44,9 +47,28 @@ def parse_sequence(lines):
     return sequence
 
 
+def get_ids_to_keep(split_file):
+    id_regexp = r'.*/(\d+_\d+_\d+_\d+).*'
+
+    def get_id(line):
+        matches = re.match(id_regexp, line)
+        return matches.groups()[0] if matches else None
+
+    with open(split_file, 'rt') as f:
+        ids = {get_id(line) for line in f}
+
+    return ids
+
+
 def main(args):
     sequences = get_sequences(args.data)
-    parsed = [parse_sequence(seq) for seq in tqdm(sequences)]
+    parsed = (parse_sequence(seq) for seq in sequences)
+
+    if args.split:
+        ids_to_keep = get_ids_to_keep(args.split)
+        parsed = filter(lambda x: x['id'] in ids_to_keep, parsed)
+
+    parsed = list(tqdm(parsed))
     with open(args.parsed_data, 'wb') as outfile:
         pickle.dump(parsed, outfile)
 
@@ -54,6 +76,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse motion data')
     parser.add_argument('data', help='path to data file (in textual format)')
+    parser.add_argument('-s', '--split', help='path to optional split file (in textual format)')
     parser.add_argument('parsed_data', help='output file with parsed data file (in Pickle format)')
     args = parser.parse_args()
     main(args)
