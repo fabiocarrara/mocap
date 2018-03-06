64d4729fc5a83287ad1e480622772d6bf34beae7
Fabio Carrara <fabio.blackdragon@gmail.com>
2018-03-06 15:51:43 +0100

    add architecture for segmentation

--- model.py
+++ model.py
@@ -1,10 +1,13 @@
+import torch
 from torch import nn
+from torch.autograd import Variable
 
 
 class MotionModel(nn.Module):
     def __init__(self, in_size, out_size, hidden=128, dropout=0.5, bidirectional=True, stack=1, layers=1, embed=0):
         super(MotionModel, self).__init__()
         self.in_size = in_size
+        self.bidirectional = bidirectional
         rnn_hidden = hidden // 2 if bidirectional else hidden
 
         self.embed = nn.Sequential(
@@ -34,6 +37,44 @@ class MotionModel(nn.Module):
             outputs, hidden = stack_lstm(outputs)
         last_out = hidden[1].view(1, -1)
         return self.classifier(last_out)
+
+    def segment(self, input):
+        input = input.view(-1, self.in_size)  # seq, data
+        if self.embed is not None:
+            input = self.embed(input)  # embed all data in the sequence
+
+        input = input.unsqueeze(1)  # seq, batch, data
+        outputs, hidden = self.lstm(input)
+        for stack_lstm in self.stack:
+            outputs, hidden = stack_lstm(outputs)
+
+        return self.classifier(outputs)
+
+    def steps_forward(self, input):
+        input = input.view(-1, self.in_size)  # seq, data
+        if self.embed is not None:
+            input = self.embed(input)  # embed all data in the sequence
+
+        input = input.unsqueeze(1)  # seq, batch, data
+        outputs, hidden = self.lstm(input)
+        for stack_lstm in self.stack:
+            outputs, hidden = stack_lstm(outputs)
+
+        ''' for bidirectional models, we have to reverse the hidden states
+            of the second direction in order to have the combined hidden state
+            for each time step
+        '''
+        if self.bidirectional:
+            seq_len = input.shape[0]
+            outputs = outputs.view(seq_len, 2, -1)
+            idx = torch.LongTensor([i for i in range(seq_len - 1, -1, -1)])
+            if outputs.is_cuda:
+                idx = idx.cuda()
+            idx = Variable(idx, requires_grad=False)
+            outputs[:, 1] = outputs[:, 1].index_select(0, idx)
+            outputs = outputs.view(seq_len, -1)
+
+        return self.classifier(outputs)
 
     def extract(self, input):
         input = input.view(-1, self.in_size)  # seq, data
